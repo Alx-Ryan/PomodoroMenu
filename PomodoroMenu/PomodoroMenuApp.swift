041ec8cd51f1e75ecab80b78cfdb067f1966d386
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-09-16 12:04:19 -0500

    Initial Commit

---
+++ PomodoroMenu/PomodoroMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroMenuApp.swift
+//  PomodoroMenu
+//
+//  Created by Alex Ryan on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
